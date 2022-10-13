6c5b926ee41e3a0fbffb2fbe7335393491cc8ff9
JorensM <jorensmerenjanu@gmail.com>
2022-10-13 13:11:29 +0300

    stuff

--- generate_match_metadata.php
+++ generate_match_metadata.php
@@ -3,14 +3,42 @@
     require_once("wp_init.php");    
     require_once("product_functions.php");
 
+    //FC Barcelona = fcbarcelona
     function cleanstr($str) {
-        return urlencode(strtolower(str_replace(" ","",preg_replace("/&([a-z])[a-z]+;/i", "$1", htmlentities($str)))));
+        $new_string = urlencode(strtolower(str_replace(" ","",preg_replace("/&([a-z])[a-z]+;/i", "$1", htmlentities($str)))));
+        $newer_string = preg_replace('/[^A-Za-z0-9\-]/', '', $new_string);
+        return $newer_string;
+    }
+
+    //FC Barcelona = FCBarcelona
+    function cleanstr2($str){
+        $new_string = urlencode(str_replace(" ","",preg_replace("/&([a-z])[a-z]+;/i", "$1", htmlentities($str))));
+        $newer_string = preg_replace('/[^A-Za-z0-9\-]/', '', $new_string);
+        return $newer_string;
+    }
+
+    function file_exists_on_url($url){
+        $file = $url;
+        $file_headers = @get_headers($file);
+        if($file_headers[0] == 'HTTP/1.1 404 Not Found') {
+            return false;
+        }
+        else {
+            return true;
+        } 
     }
 
     function generate_team_img_filenames($match){
 
         $team_1 = wp_upload_dir()["url"] . "/" . cleanstr($match["home_club"]) . ".png";
         $team_2 = wp_upload_dir()["url"] . "/" . cleanstr($match["away_club"]) . ".png";
+
+        if(!file_exists_on_url($team_1)){
+            $team_1 = wp_upload_dir()["url"] . "/" . cleanstr2($match["home_club"]) . ".png";
+        }
+        if(!file_exists_on_url($team_2)){
+            $team_2 = wp_upload_dir()["url"] . "/" . cleanstr2($match["away_club"]) . ".png";
+        }
 
         return array(
             $team_1,
