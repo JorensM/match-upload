c3425dfa80ae554c0f7d6fc975b7359b943717f5
JorensM <jorensmerenjanu@gmail.com>
2023-01-18 11:54:47 +0200

    stuff
    * changed api_keys.php imports to const.php

--- php/functions/wooGetVariations.php
+++ php/functions/wooGetVariations.php
@@ -1,7 +1,7 @@
 <?php
 
     require_once("curlGet.php");
-    require_once(__DIR__."/../../api_keys.php");
+    require_once(__DIR__."/../../const.php");
 
     /**
      * Calls the WooCommerce /products endpoint
